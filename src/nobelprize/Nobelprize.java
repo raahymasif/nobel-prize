1bdffa4564f32b880a5de17fbaf2c11e3b6d7f78
cmattinson <c.mattinson@hotmail.com>
2017-02-27 21:27:58 -0700

    Updates to UI

--- src/nobelprize/Nobelprize.java
+++ src/nobelprize/Nobelprize.java
@@ -23,6 +23,7 @@ public class Nobelprize extends Application {
         
         Scene scene = new Scene(root);
         
+        stage.setTitle("Nobel Prize");
         stage.setScene(scene);
         stage.show();
     }
