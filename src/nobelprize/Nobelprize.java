c3ace242864b0165ea67b093be0beadb96e81534
cmattinson <c.mattinson@hotmail.com>
2017-02-28 17:24:51 -0700

    Add search bars to UI and remove dependencies in project properties

--- src/nobelprize/Nobelprize.java
+++ src/nobelprize/Nobelprize.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nobelprize;
 
 import javafx.application.Application;
@@ -13,7 +8,8 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author chris
+ * @author Chris Mattinson <mattinsonc@mymacewan.ca>
+ *          Raahym Asif
  */
 public class Nobelprize extends Application {
     
