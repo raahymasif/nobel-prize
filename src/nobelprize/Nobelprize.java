0f111a3012df80dc8aecac21b0c6422290319e91
Raahym <asifr@mymacewan.ca>
2017-03-02 17:50:17 -0700

    Added functionalry for parsing from the API

--- src/nobelprize/Nobelprize.java
+++ src/nobelprize/Nobelprize.java
@@ -1,7 +1,7 @@
 package nobelprize;
 
 
-import com.sun.deploy.util.StringUtils;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,6 +9,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import org.json.*;
 
 /**
  *
@@ -31,7 +33,7 @@ public class Nobelprize extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws MalformedURLException, IOException {
+    public static void main(String[] args) throws MalformedURLException, IOException, JSONException {
         launch(args);
         URL nobelprize = new URL("http://api.nobelprize.org/v1/prize.json");
         URLConnection nobelconnection = nobelprize.openConnection();
@@ -39,9 +41,25 @@ public class Nobelprize extends Application {
     
         String inputlines;
         int numyears;
+              
+        
+        
         while ((inputlines = in.readLine()) != null){
-            System.out.println(inputlines);
+            JSONObject myjson = new JSONObject(inputlines);
+            JSONArray the_json_array = myjson.getJSONArray("prizes");
+            int size = the_json_array.length();
+            ArrayList<JSONObject> arrays = new ArrayList<JSONObject>();
+                for (int i = 0; i < size; i++){
+                    JSONObject another_json_object = the_json_array.getJSONObject(i);
+                    
+                    arrays.add(another_json_object);
+                }
+            JSONObject[] jsons = new JSONObject[arrays.size()];
+            arrays.toArray(jsons);
+            
         }
+        System.out.println(another_json_object); // see its giving an error here cuz idk how to print he map
+        //System.out.println(inputlines);
         in.close();
     }
     
